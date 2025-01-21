76d2d950563c4cdc62e1614872f2014986b1ea10
Elizabeth Donoway <elizabeth.donoway@gmail.com>
2025-01-20 18:31:17 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 USAGE=$(cat <<-END
     Usage: ./deploy.sh [OPTIONS], eg. ./deploy.sh --local --vim
     Creates ~/.zshrc and ~/.tmux.conf with location
